7fd785260662446f40bb6ad3fac0858bd2d9d647
Bruno Gustavo Busarello <150341182+brunobusarello@users.noreply.github.com>
2024-09-18 16:41:12 -0300

    Try to do a static page router

--- server.js
+++ server.js
@@ -1,23 +1,35 @@
 import { fastify } from 'fastify'
 import { DatabasePostgres } from './database-postgres.js'
 import cors from '@fastify/cors'
-import formbody from '@fastify/formbody'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import fastifyStatic from '@fastify/static'
 //import { DatabaseMemory} from './database-memory.js'
 
-const server = fastify()
+const server = fastify({ logger: true })
 //const dataBase = new DatabaseMemory
 const dataBase = new DatabasePostgres
+
+// Obter __dirname com ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Registrar o plugin fastify-static para servir arquivos estáticos
+server.register(fastifyStatic, {
+  root: path.join(__dirname, 'public'),
+  prefix: '/front/', // Prefixo opcional
+});
 
 server.register(cors, {
     origin: '*' // Permitir todas as origens
 })
 
-server.register(formbody)
-
 server.get('/videos', async(request, reply) => {
     const search = request.query.search
 
     const video = await dataBase.list(search)
+
+    reply.sendFile('videos.html')
 
     return video
 })
